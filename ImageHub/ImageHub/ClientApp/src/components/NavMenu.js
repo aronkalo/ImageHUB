c444faf5a9b3b1e9763212ffd8542f0fe1a6983d
Egres Kata <egres.kata0@gmail.com>
2021-03-16 23:18:29 +0100

    feat: CSS added to Home

--- ImageHub/ImageHub/ClientApp/src/components/NavMenu.js
+++ ImageHub/ImageHub/ClientApp/src/components/NavMenu.js
@@ -3,6 +3,7 @@ import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLi
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import './NavMenu.css';
+import logo from './images/logo-horizontal.png'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -27,7 +28,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">ImageHub</NavbarBrand>
+                    <NavbarBrand tag={Link} to="/"><img src={logo} /></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
