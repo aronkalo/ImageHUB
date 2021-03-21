1d2ecd4fcbd620ab0ba38e585abfa887203c3171
EBO7OO <turoczidavid05@gmail.com>
2021-03-21 12:02:42 +0100

    feat: popup added to personalFeed

--- ImageHub/ImageHub/ClientApp/src/components/PersonalFeed.js
+++ ImageHub/ImageHub/ClientApp/src/components/PersonalFeed.js
@@ -1,4 +1,4 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component, useEffect, useRef } from 'react';
 import authService from './api-authorization/AuthorizeService'
 import './PersonalFeed.css';
 import setting_img from './images/settings.png'
@@ -7,21 +7,92 @@ import like_img from './images/white-like.png'
 import { Link } from 'react-router-dom';
 import { ApplicationPaths } from './api-authorization/ApiAuthorizationConstants';
 import { NavLink } from 'reactstrap';
+import { useState } from 'react'
+import { data } from 'jquery';
+
+function Pictures(props) {
+    const [buttonPopup, setButtonPopup] = useState(false)
+    const [popupData, setPopupData] = useState()
+    let popupRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener("mousedown", (event) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                setButtonPopup(false);
+            }
+        });
+    });
+
+    function Popup(props) {
+        return (props.trigger) ? (
+            <div class="popup">
+                <div class="popup-inner" ref={popupRef}>
+                    <img src={"https://localhost:44335/services/files/" + props.data.identifier} class="gallery-image" alt=""></img>
+                </div>
+            </div>
+        ) : null;
+    };
+
+    function OnOrTwoPicture(props) {
+        if (props.count == 1) {
+            return (
+                <div class="gallery-item" tabindex="0"></div>
+            );
+        }
+        else if (props.count == 2) {
+            return (
+                <>
+                    <div class="gallery-item" tabindex="0"></div>
+                    <div class="gallery-item" tabindex="0"></div>
+                </>
+            );
+        }
+        else
+            return null
+    }
+
+    return (
+        <>
+            {
+                props.datas.map((data) =>
+                    <div class="gallery-item" tabindex="0" onClick={() => { setButtonPopup(true); setPopupData(data); }}>
+                        <img src={"https://localhost:44335/services/files/" + data.identifier} class="gallery-image" alt=""></img>
+                        <div class="gallery-item-type">
+                            <img></img>
+                        </div>
+                        <div class="gallery-item-info">
+                            <ul>
+                                <li class="gallery-item-likes"><img class="like-img" src={like_img}></img> 30</li>
+                                <li class="gallery-item-comments"><img class="comment-img" src={comment_img}></img> 2</li>
+                            </ul>
+                        </div>
+                    </div>
+                )
+            }
+            <OnOrTwoPicture count={props.datas.postcount}></OnOrTwoPicture>
+            <div>
+                <Popup trigger={buttonPopup} setTrigger={setButtonPopup} data={popupData}>
+                </Popup>
+            </div>
+        </>
+    );
+}
 
 export class PersonalFeed extends Component {
     static displayName = PersonalFeed.name;
     static profilePath = `${ApplicationPaths.Profile}`;
 
+
     constructor(props) {
         super(props)
-        this.state = { loading: true };
+        this.state = { medias: [], loading: true };
     }
 
     componentDidMount() {
         this.PersonalDatas();
     }
 
-    static renderHeader() {
+    static renderHeader(datas) {
         return (
             <header>
                 <div class="container">
@@ -38,7 +109,7 @@ export class PersonalFeed extends Component {
                         </div>
                         <div class="profile-stats">
                             <ul>
-                                <li><span class="profile-stat-count">{this.PostCount()}</span> posts</li>
+                                <li><span class="profile-stat-count">{datas.postcount}</span> posts</li>
                                 <li><span class="profile-stat-count">{this.followersCount()}</span> followers</li>
                                 <li><span class="profile-stat-count">{this.followingCount()}</span> following</li>
                             </ul>
@@ -52,8 +123,8 @@ export class PersonalFeed extends Component {
         );
     }
 
-    static renderMain() {
-        if (this.PostCount() == 0) {
+    static renderMain(datas) {
+        if (datas.postcount == 0) {
             return (<></>);
         }
         else {
@@ -61,7 +132,7 @@ export class PersonalFeed extends Component {
                 <main>
                     <div class="container">
                         <div class="gallery">
-                            {this.renderPicture()}
+                            <Pictures datas={datas}></Pictures>
                         </div>
                         <div class="loader">
                         </div>
@@ -71,58 +142,13 @@ export class PersonalFeed extends Component {
         }
     }
 
-    static renderPicture() {
-        return (<>
-            <div class="gallery-item" tabindex="0">
-                <img src="https://images.unsplash.com/photo-1423012373122-fff0a5d28cc9?w=500&h=500&fit=crop" class="gallery-image" alt=""></img>
-                <div class="gallery-item-type">
-                    <img></img>
-                </div>
-                <div class="gallery-item-info">
-                    <ul>
-                        <li class="gallery-item-likes"><img class="like-img" src={like_img}></img> 30</li>
-                        <li class="gallery-item-comments"><img class="comment-img" src={comment_img}></img> 2</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="gallery-item" tabindex="0">
-                <img src="https://images.unsplash.com/photo-1423012373122-fff0a5d28cc9?w=500&h=500&fit=crop" class="gallery-image" alt=""></img>
-                <div class="gallery-item-type">
-                    <img></img>
-                </div>
-                <div class="gallery-item-info">
-                    <ul>
-                        <li class="gallery-item-likes"><img class="like-img" src={like_img}></img> 30</li>
-                        <li class="gallery-item-comments"><img class="comment-img" src={comment_img}></img> 2</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="gallery-item" tabindex="0">
-                <img src="https://images.unsplash.com/photo-1423012373122-fff0a5d28cc9?w=500&h=500&fit=crop" class="gallery-image" alt=""></img>
-                <div class="gallery-item-type">
-                    <img></img>
-                </div>
-                <div class="gallery-item-info">
-                    <ul>
-                        <li class="gallery-item-likes"><img class="like-img" src={like_img}></img> 30</li>
-                        <li class="gallery-item-comments"><img class="comment-img" src={comment_img}></img> 2</li>
-                    </ul>
-                </div>
-            </div></>
-        );
-    }
-
-    static GetFeed() {
+    static GetFeed(datas) {
         return (
             <>
-                {this.renderHeader()}
-                {this.renderMain()}
+                {this.renderHeader(datas)}
+                {this.renderMain(datas)}
             </>
         );
-    }
-
-    static PostCount() {
-        return 3;
     }
 
     static followersCount() {
@@ -134,9 +160,14 @@ export class PersonalFeed extends Component {
     }
 
     render() {
+        if (!this.state.loading) {
+            this.state.medias.postcount = this.state.medias.length
+        }
+
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : PersonalFeed.GetFeed();
+            : PersonalFeed.GetFeed(this.state.medias);
+
 
         return (
             <div>
@@ -147,7 +178,10 @@ export class PersonalFeed extends Component {
 
     async PersonalDatas() {
         const token = await authService.getAccessToken();
-
-        this.setState({ loading: false })
+        const response = await fetch('services/medias/', {
+            headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+        });
+        const data = await response.json();
+        this.setState({ medias: data, loading: false });
     }
 }
